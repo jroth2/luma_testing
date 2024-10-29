885f4ba2ff8c521613c549de36b23a57027fdee5
Joe Roth <jroth213@gmail.com>
2024-10-28 22:09:10 -0500

    adds more examples

---
+++ demo_opts.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2014-2022 Richard Hull and contributors
+# See LICENSE.rst for details.
+
+import sys
+import logging
+
+from luma.core import cmdline, error
+from luma.oled.device import ssd1309
+from luca.core.interface.serial import spi
+
+def get_device(actual_args=None):
+    serial = spi(bus_speed_hz=1000000)
+    device = ssd1309(serial,rotate=0)    
+    return device
+
